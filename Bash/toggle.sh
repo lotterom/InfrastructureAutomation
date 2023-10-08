4b4a85f48c9ccf249c0ac963e26a828e8834fecd
lotterom <lotte.rombaut@student.howest.be>
2023-10-08 12:11:42 +0200

    opstelling bash script

---
+++ Bash/toggle.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Hoofdmenu
+echo "1. Schakel DHCP in"
+echo "2. Stel statisch IP in"
+
+read -p "Maak een keuze (1 of 2): " choice
+
+case $choice in
+    1)
+        enable_dhcp
+        ;;
+    2)
+        set_static_ip
+        ;;
+    *)
+        echo "Ongeldige keuze. Voer 1 of 2 in."
+        ;;
+esac

26fd8b470f6718845491d3aeb78bbb1e0455baa4
lotterom <lotte.rombaut@student.howest.be>
2023-10-15 14:52:54 +0200

    alter dhcpd.config met nieuwe data

--- Bash/dhcp.sh
+++ Bash/dhcp.sh
@@ -7,3 +7,10 @@ if [ ! -f "$input_file" ]; then
     echo "Ongeldig bestand: $input_file bestaat niet."
     exit 1
 fi
+
+#overlooop csv en voeg config toe aan dhcpd.conf
+while IFS=, read -r subnet start end; do
+    echo "subnet $subnet netmask 255.255.255.0 {" >> /etc/dhcp/dhcpd.conf
+    echo "  range $start $end;" >> /etc/dhcp/dhcpd.conf
+    echo "}" >> /etc/dhcp/dhcpd.conf
+done < "$input_file"
